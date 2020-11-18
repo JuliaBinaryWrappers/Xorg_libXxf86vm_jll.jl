55767c6287b6fb11558ef6b3592d602c7bdc0acd
jlbuild <juliabuildbot@gmail.com>
2020-11-18 12:45:54 +0000

    Xorg_libXxf86vm_jll build 1.1.4+3

--- src/wrappers/aarch64-linux-gnu.jl
+++ src/wrappers/aarch64-linux-gnu.jl
@@ -2,57 +2,15 @@
 export libXxf86vm
 
 using Xorg_libXext_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-
-# Relative path to `libXxf86vm`
-const libXxf86vm_splitpath = ["lib", "libXxf86vm.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libXxf86vm_path = ""
-
-# libXxf86vm-specific global declaration
-# This will be filled out by __init__()
-libXxf86vm_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libXxf86vm = "libXxf86vm.so.1"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("Xorg_libXxf86vm")
+JLLWrappers.@declare_library_product(libXxf86vm, "libXxf86vm.so.1")
 function __init__()
-    global artifact_dir = abspath(artifact"Xorg_libXxf86vm")
+    JLLWrappers.@generate_init_header(Xorg_libXext_jll)
+    JLLWrappers.@init_library_product(
+        libXxf86vm,
+        "lib/libXxf86vm.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Xorg_libXext_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Xorg_libXext_jll.LIBPATH_list,))
-
-    global libXxf86vm_path = normpath(joinpath(artifact_dir, libXxf86vm_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libXxf86vm_handle = dlopen(libXxf86vm_path)
-    push!(LIBPATH_list, dirname(libXxf86vm_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(LIBPATH_list, ':')
-
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
